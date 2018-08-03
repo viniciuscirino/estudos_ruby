579ec317c423d031db9668b24b6233c11d6852a7
viniciuscirino <viniciuscirino@outlook.com>
2018-08-02 21:52:29 -0300

    commit das 21:52

--- estudos_ruby/aula.rb
+++ estudos_ruby/aula.rb
@@ -1,4 +1,11 @@
 require 'byebug'
-s = "vinicius"
-debugger
+a = "vinicius"
+# debugger
 x = 'manollo'
+puts "#{self}"
+puts "Informe a Mensagem"
+msg = gets
+public
+def mostrar_algo(msg = "Nada")
+ puts "#{self.mostrar_algo}"
+end
