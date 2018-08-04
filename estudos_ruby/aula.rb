0dda65cf90a48b1942b4b0668504f46c166d2ee8
viniciuscirino <viniciuscirino@outlook.com>
2018-08-03 21:09:32 -0300

    commit 21:08 do dia 3 de agosto

--- estudos_ruby/aula.rb
+++ estudos_ruby/aula.rb
@@ -1,11 +1,24 @@
 require 'byebug'
-a = "vinicius"
-# debugger
-x = 'manollo'
-puts "#{self}"
-puts "Informe a Mensagem"
-msg = gets
-public
-def mostrar_algo(msg = "Nada")
- puts "#{self.mostrar_algo}"
+# a = "vinicius"
+# # debugger
+# x = 'manollo'
+# puts "#{self}"
+# puts "Informe a Mensagem"
+# msg = gets
+# public
+# def mostrar_algo(msg = "Nada")
+#  puts "#{self.mostrar_algo}"
+# end
+class Pessoa
+  class << self
+    attr_accessor :nome, :cpf ,:rg ,:endereco, :telefone
+  end
+end
+module Mostrar
+    def mostrar_pessoa
+        pessoa = Pessoa
+        puts "#{:nome},#{:cpf}, #{:rg}, #{:endereco}, #{:telefone}"
+        puts "######################################################"
+    end 
+    debugger
 end
